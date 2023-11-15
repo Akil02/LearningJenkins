d9638f14c725c0f1a93cce291fb4f97137efe2ea
A AKIL GANESH <akilganesh3@gmail.com>
2023-11-15 07:56:40 +0530

    To practice jenkins

---
+++ src/test/java/SampleProject/Sample/BasicSample.java
@@ -0,0 +1,22 @@
+package SampleProject.Sample;
+
+import org.testng.annotations.Test;
+
+public class BasicSample {
+	
+	@Test
+	public void  SmapleTest1() {
+		System.out.println("This is for sample test 1");
+	}
+	
+	@Test
+	public void  SmapleTest2() {
+		System.out.println("This is for sample test 2");
+	}
+	
+	@Test
+	public void  SmapleTest3() {
+		System.out.println("This is for sample test 3");
+	}
+
+}
